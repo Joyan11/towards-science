9d25ec6ceca28004f9d7e72ebc967a4b15cf674f
joyan11 <joyan.s03@gmail.com>
2021-04-29 22:44:59 +0530

    feat: new context

--- src/index.js
+++ src/index.js
@@ -4,18 +4,21 @@ import "./index.css";
 import App from "./App";
 import { WatchListProvider } from "./contexts/watchlist-context";
 import { LikeProvider } from "./contexts/like-context";
+import { GeneralContextProvider } from "./contexts/general-context";
 import { PlaylistProvider } from "./contexts/playlist-context";
 import { BrowserRouter as Router } from "react-router-dom";
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <WatchListProvider>
-        <LikeProvider>
-          <PlaylistProvider>
-            <App />
-          </PlaylistProvider>
-        </LikeProvider>
-      </WatchListProvider>
+      <GeneralContextProvider>
+        <WatchListProvider>
+          <LikeProvider>
+            <PlaylistProvider>
+              <App />
+            </PlaylistProvider>
+          </LikeProvider>
+        </WatchListProvider>
+      </GeneralContextProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
