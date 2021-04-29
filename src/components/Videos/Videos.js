005e32d01ee8af124beee7cfd0a6319b25fbec21
joyan11 <joyan.s03@gmail.com>
2021-04-29 22:57:37 +0530

    feat: category

--- src/components/Videos/Videos.js
+++ src/components/Videos/Videos.js
@@ -1,15 +1,31 @@
 import React from "react";
 import "../../css/videos.css";
 import { VideoCard } from "./VideoCard";
-import { data } from "../../data/data";
 import { PlaylistModal } from "./PlaylistModal";
+import { useGeneralContext } from "../../contexts/general-context";
+import { CategoryBar } from "../Categories/CategoryBar";
 export function Videos() {
   // console.log(showPlaylistModal);
+  const { videos, videoFilter } = useGeneralContext();
+
+  const filterByCategory = (videos, videofilter) => {
+    if (videofilter === "all") {
+      return videos;
+    } else {
+      return videos.filter((item) => item.category === videofilter);
+    }
+  };
+
+  const filteredItems = filterByCategory(videos, videoFilter);
+  console.log(filteredItems);
   return (
     <section className="video-section">
-      {data.map((videos) => {
-        return <VideoCard key={videos.id} videos={videos} />;
-      })}
+      <CategoryBar />
+      <div className="video-page">
+        {filteredItems.map((videos) => {
+          return <VideoCard key={videos.id} videos={videos} />;
+        })}
+      </div>
 
       <PlaylistModal />
     </section>
