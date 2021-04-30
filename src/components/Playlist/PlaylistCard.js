9c3fc44c548a6fb3b37a9e4f0a3acd7c9f5f6b7a
joyan11 <joyan.s03@gmail.com>
2021-04-30 11:41:37 +0530

    refactor: fixed playlist layout

--- src/components/Playlist/PlaylistCard.js
+++ src/components/Playlist/PlaylistCard.js
@@ -7,35 +7,39 @@ export const PlaylistCard = () => {
   const playlistData = playList.find((item) => item.id === id);
 
   return (
-    <div className="playlist-card-container">
+    <div className="playlist-card-section">
       {playlistData.list.map((playlistitem) => {
         return (
-          <div
-            kay={playlistitem.id}
-            className="card card--verticle card--l video-card playlist-card">
-            <figure className="card--image">
-              <span
-                onClick={() =>
-                  dispatchplaylist({
-                    type: "REMOVE_FROM_PLAYLIST",
-                    playlistId: id,
-                    videoData: playlistitem.id,
-                  })
-                }>
+          <div className="playlist-card-container">
+            <div
+              kay={playlistitem.id}
+              className="card card--verticle card--l video-card">
+              <figure className="card--image">
+                <span
+                  onClick={() =>
+                    dispatchplaylist({
+                      type: "REMOVE_FROM_PLAYLIST",
+                      playlistId: id,
+                      videoData: playlistitem.id,
+                    })
+                  }>
+                  {" "}
+                  <ion-icon
+                    class="card--dismiss"
+                    name="close-circle"></ion-icon>
+                </span>{" "}
+                <Link to={`/${playlistitem.id}`} className="link">
+                  <img
+                    src={`https://img.youtube.com/vi/${playlistitem.id}/mqdefault.jpg`}
+                    alt={playlistitem.name}
+                  />{" "}
+                </Link>
+              </figure>
+              <div className="card--body">
                 {" "}
-                <ion-icon class="card--dismiss" name="close-circle"></ion-icon>
-              </span>{" "}
-              <Link to={`/${playlistitem.id}`} className="link">
-                <img
-                  src={`https://img.youtube.com/vi/${playlistitem.id}/mqdefault.jpg`}
-                  alt={playlistitem.name}
-                />{" "}
-              </Link>
-            </figure>
-            <div className="card--body">
-              {" "}
-              <span clasNames="card--title">{playlistitem.name}</span>
-              <p className="card--text"> {playlistitem.category} </p>
+                <span clasNames="card--title">{playlistitem.name}</span>
+                <p className="card--text"> {playlistitem.category} </p>
+              </div>
             </div>
           </div>
         );
