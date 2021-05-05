40b6514c83e9d913519ba0d524e9d2bd78a388b5
joyan11 <joyan.s03@gmail.com>
2021-05-05 18:00:06 +0530

    refactor: added route to logo

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Logo from "../../assets/images/atom.svg";
 import { useGeneralContext } from "../../contexts/general-context";
 
@@ -14,11 +15,13 @@ export function Navbar() {
             <ion-icon name="menu-outline"></ion-icon>
           </span>
           <p>
-            Towards
-            <span>
-              <img alt="towards science logo" srcSet={Logo} />
-            </span>
-            Science
+            <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
+              Towards
+              <span>
+                <img alt="towards science logo" srcSet={Logo} />
+              </span>
+              Science
+            </Link>
           </p>
         </div>
       </nav>
